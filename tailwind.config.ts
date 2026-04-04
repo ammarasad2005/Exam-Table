56ee6e60048f409f90035f86b68381e3bccbf745
Muhammad Ammar Asad <ammarasad321993@gmail.com>
2026-04-04 18:39:21 +0500

    update tailwind config to support dark mode theme toggle for logo inversion

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'tailwindcss';
 
 export default {
   content: ['./src/**/*.{ts,tsx}'],
+  darkMode: ['selector', '[data-theme="dark"]'],
   theme: {
     extend: {
       fontFamily: {
